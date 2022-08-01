c6becb21c0afe5de8b8e3260797d871fe05f631b
Alex <alex@Alexs-MacBook-Air.local>
2022-08-01 14:52:21 -0300

    adding landing page

---
+++ components/layouts/LandingLayout.tsx
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import React from "react";
+import useMediaQuery from "../../hooks/useMediaQuery";
+
+export default function LandingLayout({
+  children,
+  title,
+  meta,
+}: {
+  children: React.ReactNode;
+  title: string;
+  meta: string;
+}) {
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
+
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={meta} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {isDesktop ? <Navbar /> : <Sidebar />}
+      <div className="flex-1">{children}</div>
+      <footer className="bg-slate-100 w-full py-4 flex justify-center">
+        This is the footer.
+      </footer>
+    </div>
+  );
+}
+
+const Sidebar = () => {
+  return (
+    <header className="bg-slate-100 w-full py-4 flex justify-center">
+      <nav>This is the Sidebar</nav>
+    </header>
+  );
+};
+const Navbar = () => {
+  return (
+    <header className="bg-slate-100 w-full py-4 flex justify-center">
+      <nav>This is the Navbar</nav>
+    </header>
+  );
+};
